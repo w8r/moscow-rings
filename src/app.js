f28b6c3b5e8d8d13e63ffa2f208f84864dd7a6c9
w8r <info@w8r.name>
2015-10-15 16:59:33 +0200

    EQUIDISTANT BUFFERING WORKS

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ import turf from 'turf';
 import gjutils from 'geojson-utils';
 import geom from 'leaflet-geometryutil';
 import 'leaflet-hash';
-import crs from './projection';
+import { EPSG3857, equidistant, abstractEquidistant } from './projection';
 import { project, unproject, buffer } from './geojson';
+import Polygon from 'polygon';
+import Vec2 from 'vec2';
 
 global.turf = turf;
 
@@ -24,12 +26,21 @@ const POSITION_STYLE = {
   radius: 6
 }
 
+function euclidianDistance(coord1, coord2) {
+  return Math.sqrt(
+    Math.pow(coord1[0] - coord2[0], 2) +
+    Math.pow(coord1[1] - coord2[1], 2)
+  );
+}
+
 function sdistance (latlon1, latlon2, crs) {
   let p1 = crs.project(latlon1);
   let p2 = crs.project(latlon2);
 
   return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 }
+
+
 
 function ringStyle(feature) {
   return {
@@ -119,14 +130,23 @@ export default class App {
 
     //console.time('proj');
     this._projected = project(data, this._map.options.crs);
+
+    this._equidistant = project(data, equidistant);
+
+    this._equidistant.features.forEach((f) => {
+      f.properties.centroid =
+        gjutils.centroid(f.geometry).coordinates;
+    });
     //console.timeEnd('proj');
 
     //console.time('unproj');
     //let unprojected = unproject(projected, this._map.options.crs);
     //console.timeEnd('unproj');
 
-    if(!this._positioned) {
-      map.fitBounds(this._geojson.getBounds(), {padding: [20, 20]});
+    if (!this._positioned) {
+      map.fitBounds(this._geojson.getBounds(), {
+        padding: [20, 20]
+      });
     }
     this._geojson.addTo(map);
     this._index = leafletKnn(this._geojson);
@@ -152,17 +172,42 @@ export default class App {
 
   _calculateDistances(latlng) {
     let intersections = [];
+    let eintersections = [];
     let point = turf.point([latlng.lng, latlng.lat]);
+    let epoint = turf.point(abstractEquidistant.project([latlng.lng, latlng.lat]));
+
+    console.log('you clicked', JSON.stringify(epoint));
+
     this._geojson.eachLayer((layer) => {
       let f = layer.feature;
+      let ef = this._equidistant.features.filter((e) => {
+        return e.properties.id === f.properties.id;
+      })[0];
+
+      // console.log(f, ef);
+
       let centroid = L.latLng(f.properties.centroid.slice().reverse());
 
       let intersects = gjutils.lineStringsIntersect(
         { coordinates: f.geometry.coordinates[0] },
-        { coordinates: [f.properties.centroid.slice(), [latlng.lng, latlng.lat]] }
+        { coordinates: [f.properties.centroid.slice(), point.geometry.coordinates] }
       );
 
-      if(intersects) {
+      let eintersects = gjutils.lineStringsIntersect(
+        { coordinates: ef.geometry.coordinates[0] },
+        { coordinates: [ef.properties.centroid.slice(), epoint.geometry.coordinates] }
+      );
+
+      // (eintersects || []).forEach((p) => {
+      //   console.log('intersect', p.coordinates, abstractEquidistant.unproject(p.coordinates.slice().reverse()));
+      //   L.circleMarker(
+      //     abstractEquidistant.unproject(p.coordinates.slice().reverse()).reverse(), {
+      //       radius: 5,
+      //       color: '#fff'
+      //     }).addTo(this._map);
+      // }, this);
+
+      if (intersects) {
         intersects = intersects.pop();
         intersects.coordinates.reverse();
         L.circleMarker(intersects.coordinates.slice().reverse(), {
@@ -175,12 +220,28 @@ export default class App {
 
       let nearest = turf.pointOnLine(
         turf.linestring(f.geometry.coordinates[0]), point);
+
+      let epolygon = new Polygon(ef.geometry.coordinates[0]);
+      let enearest = new Polygon(ef.geometry.coordinates[0])
+         .closestPointTo(Vec2.apply(null, epoint.geometry.coordinates)).toArray();
+
+      L.circleMarker(abstractEquidistant.unproject(enearest).slice().reverse(), {
+          radius: 8,
+          color: COLORS[f.properties.id]
+        }).addTo(this._intersects);
+
+      enearest = turf.point(enearest);
+
       L.circleMarker(nearest.geometry.coordinates.slice().reverse(), {
           radius: 2,
           color: COLORS[f.properties.id]
         }).addTo(this._intersects);
+
       nearest.properties.feature = f;
+      enearest.properties.features = ef;
+
       intersections.push(nearest);
+      eintersections.push(enearest);
 
       // visual
       // L.polyline([latlng, centroid], {
@@ -189,7 +250,7 @@ export default class App {
     }, this);
 
     this._info.innerHTML = '<pre>' +
-        JSON.stringify(intersections.map((i) => {
+        JSON.stringify(intersections.map((i, index) => {
           let distance = turf.distance(
               point, turf.point(i.geometry.coordinates.slice()),
               "kilometers"
@@ -200,17 +261,24 @@ export default class App {
             map.options.crs
           ) / 1000;
 
+
           let ldistance = L.latLng(i.geometry.coordinates.slice().reverse())
             .distanceTo(L.latLng(point.geometry.coordinates.slice().reverse())) / 1000;
 
-          console.log(distance, sqdistance, ldistance);
-          //distance = sqdistance;
-          //distance = ldistance;
+          // console.log(distance, sqdistance, ldistance);
+          // distance = sqdistance;
+          // distance = ldistance;
+          let edistance = euclidianDistance(
+            eintersections[index].geometry.coordinates,
+            epoint.geometry.coordinates);
 
           if (turf.inside(point, i.properties.feature)) {
             distance = -distance;
+            edistance = -edistance;
           }
           i.properties.distance = distance;
+          eintersections[index].properties.distance = edistance;
+
 
           return {
             [i.properties.feature.properties.name] :
@@ -219,31 +287,59 @@ export default class App {
         }), 0, 2) +
       '</pre>';
 
-    L.Util.requestAnimFrame(() => this._calculateBuffers(intersections), this);
+    L.Util.requestAnimFrame(() => this._calculateBuffers(intersections, eintersections), this);
   }
 
-  _calculateBuffers(intersections) {
+  _calculateBuffers(intersections, eintersections) {
     let buffers = [];
+    let ebuffers = [];
+
     intersections.forEach((i, index) => {
       if (i.properties.distance > 0) {
         let projectedFeature = this._projected.features[index];
         let feature = i.properties.feature;
-        console.log(projectedFeature.properties.name, feature.properties.name, i.properties.distance * 1000);
-        buffers = buffers.concat(
+
+        let eprojectedFeature = this._equidistant.features[index];
+
+        // console.log(projectedFeature.properties.name,
+        //   feature.properties.name, i.properties.distance * 1000);
+
+        // console.log(eprojectedFeature,
+        //     eintersections[index].properties.distance)
+
+
+
+        ebuffers = ebuffers.concat(
           buffer(
-            projectedFeature,
-            i.properties.distance * 2000
-          ).features
+             eprojectedFeature,
+             eintersections[index].properties.distance
+           ).features
           .map((f) => {
-            f.properties.id = i.properties.feature.properties.id;
-            return f;
-          }));
+             f.properties.id = i.properties.feature.properties.id;
+             return f;
+        }));
+
+        // buffers = buffers.concat(
+        //   buffer(
+        //     projectedFeature,
+        //     i.properties.distance * 2000
+        //   ).features
+        //   .map((f) => {
+        //     f.properties.id = i.properties.feature.properties.id;
+        //     return f;
+        //   }));
       }
     }, this);
-    buffers = turf.featurecollection(buffers);
-    buffers = unproject(buffers, map.options.crs);
+
+    //buffers = turf.featurecollection(buffers);
+    //buffers = unproject(buffers, map.options.crs);
+
+    ebuffers = turf.featurecollection(ebuffers);
+    ebuffers = unproject(ebuffers, equidistant);
+
+
     this._buffers.clearLayers();
-    this._buffers.addData(buffers);
+    this._buffers.addData(ebuffers);
   }
 
 }
--- src/projection.js
+++ src/projection.js
@@ -1,10 +1,37 @@
 const L = global.L || require('leaflet');
 
+import d3 from 'd3';
+
+const EARTH_RADIUS =  6378137;
+
+const d3Equidistant = d3.geo.azimuthalEquidistant().scale(EARTH_RADIUS);
+
 L.CRS.EPSG3857.unproject = function (point) {
-  const earthRadius = 6378137;
   return this.projection.unproject(
-    point.multiplyBy(1 / earthRadius)
+    point.multiplyBy(1 / EARTH_RADIUS)
   );
 };
 
-export default L.CRS.EPSG3857;
+export let abstractEquidistant = {
+
+  project(coords) {
+    return d3Equidistant(coords);
+  },
+
+  unproject(coords) {
+    return d3Equidistant.invert(coords);
+  }
+}
+
+export let equidistant = {
+
+  project (latlng) {
+    return L.point(d3Equidistant([latlng.lng, latlng.lat]));
+  },
+
+  unproject (point) {
+    return L.latLng(d3Equidistant.invert([point.x, point.y]).reverse())
+  }
+};
+
+export let EPSG3857 = L.CRS.EPSG3857;
