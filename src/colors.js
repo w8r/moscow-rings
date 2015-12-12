eefef9273d89528fd432ba72204c5cde69afce5e
w8r <info@w8r.name>
2015-12-12 13:47:13 +0100

    build

--- src/colors.js
+++ src/colors.js
@@ -1,17 +1,17 @@
 const COLORS = {
-    0: '#FF0000', // red square
-    1: '#FF7F00', // kremlin,
-    2: '#FFFF00', // boulevard
-    3: '#00FF00', // garden ring
-    4: '#00FF33', // third ring
-    5: '#0088FF'  // MKAD
+    // 0: '#FF0000', // red square
+    // 1: '#FF7F00', // kremlin,
+    // 2: '#FFFF00', // boulevard
+    // 3: '#00FF00', // garden ring
+    // 4: '#00FF33', // third ring
+    // 5: '#0088FF'  // MKAD
 
-    // 0: '#A10702', // red square
-    // 1: '#A23E48', // kremlin,
-    // 2: '#3D8C7D', // boulevard
-    // 3: '#D66853', // garden ring
-    // 4: '#E9C46A', // third ring
-    // 5: '#006992'  // MKAD
+    0: '#A10702', // red square
+    1: '#A23E48', // kremlin,
+    2: '#3D8C7D', // boulevard
+    3: '#D66853', // garden ring
+    4: '#E9C46A', // third ring
+    5: '#006992'  // MKAD
 }
 
 export default COLORS;
